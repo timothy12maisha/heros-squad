50be5f34b6999a8bdc3321556f6fe7da6037192c
timothy12maisha <timomaisha@gmail.com>
2020-02-02 20:56:17 +0300

    added changes the image resources path

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,63 @@
+import java.util.Map;
+import java.util.HashMap;
+import java.util.ArrayList;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import static spark.Spark.*;
+import spark.ModelAndView;
+
+
 public class App {
+    public static void main (String[] args){
+        staticFileLocation("/public");
+        post("/posts/new", (request, response) -> { //URL to make new post on POST route
+            Map<String, Object> model = new HashMap<String, Object>();
+            String content = request.queryParams("content");
+            hero newHero = new hero(content);
+            return new ModelAndView(model, "success.hbs");
+        }, new HandlebarsTemplateEngine());
+        get("/", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            ArrayList<hero> heros = hero.getAll();
+            model.put("heros", heros);
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+        get("/heros/new", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "post-form.hbs");
+        }, new HandlebarsTemplateEngine());
+        get("/heros/:id", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            int idOfPostToFind = Integer.parseInt(req.params("id")); //pull id - must match route segment
+            hero foundPost = hero.findById(idOfPostToFind); //use it to find hero
+            model.put("post", foundPost); //add it to model for template to display
+            return new ModelAndView(model, "post-detail.hbs"); //individual post page.
+        }, new HandlebarsTemplateEngine());
+        get("/posts/:id/update", (req,res)->{
+            Map<String, Object> model = new HashMap<>();
+            int idOfPostToEdit = Integer.parseInt(req.params("id"));
+            hero editHero = hero.findById(idOfPostToEdit);
+            model.put("editPost", editHero);
+            return new ModelAndView(model, "post-form.hbs");
+        } , new HandlebarsTemplateEngine());
+        post("/posts/:id/update", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            String newContent = req.queryParams("content");
+            int idOfPostToEdit = Integer.parseInt(req.params("id"));
+            hero editHero = hero.findById(idOfPostToEdit);
+            editHero.update(newContent); //don't forget me
+            return new ModelAndView(model, "success.hbs");
+        }, new HandlebarsTemplateEngine());
+        get("/posts/:id/delete", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            int idOfPostToDelete = Integer.parseInt(req.params("id")); //pull id - must match route segment
+            hero deleteHero = hero.findById(idOfPostToDelete); //use it to find post
+            deleteHero.deleteHeros();
+            return new ModelAndView(model, "success.hbs");
+        }, new HandlebarsTemplateEngine());
+        get("/posts/delete", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            hero.clearAllHeros();
+            return new ModelAndView(model, "success.hbs");
+        }, new HandlebarsTemplateEngine());
+    }
 }
--- src/main/java/hero.java
+++ src/main/java/hero.java
@@ -1,4 +1,38 @@
-
+import java.util.ArrayList;
 
 public class hero {
+    private String content;
+    private static ArrayList<hero> instances = new ArrayList<>();
+    private boolean registered;
+    private int id;
+    public hero (String content){
+        this.content = content;
+        this.registered = false;
+        instances.add(this);
+        this.id = instances.size();
+    }
+    public String getContent() {
+        return content;
+    }
+    public static ArrayList<hero> getAll(){
+        return instances;
+    }
+    public static void clearAllHeros(){
+        instances.clear();
+    }
+    public boolean getRegistered(){
+        return this.registered;
+    }
+    public int getId() {
+        return id;
+    }
+    public static hero findById(int id){
+        return instances.get(id-1);
+    }
+    public void update(String content) {
+        this.content = content;
+    }
+    public void deleteHeros(){
+        instances.remove(id-1);
+    }
 }
