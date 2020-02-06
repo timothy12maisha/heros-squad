6e6935d169d83de2248d3c72e302dc9d0ec4d8ef
timothy12maisha <timomaisha@gmail.com>
2020-02-06 11:52:26 +0300

    included Profile

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,7 +9,15 @@ import static spark.Spark.get;
 import spark.ModelAndView;
 
 public class App {
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
     public static void main (String[] args){
+        port(getHerokuAssignedPort());
         staticFileLocation("/public");
         post("/heros/new", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
